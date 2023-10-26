480796c4198c92484f29b06bdc19fa8e05e0305f
[Demo] <[pnhaiyen520@gmail.com]>
2023-10-26 14:28:15 +0700

    Add /pages/forgotPassword, /styles/forgotPassword, /scripts/forgotPassword

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ function route(app) {
     // register
     app.use('/register', (req, res) => { res.render('./pages/register.ejs') })
     app.use('/login', (req, res) => { res.render('./pages/login.ejs') })
+    app.use('/forgotPassword', (req, res) => { res.render('./pages/forgotPassword.ejs') })
     app.use('/', siteRouter)
 }
 
