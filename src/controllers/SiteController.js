c416e898749fa61766a10b5a9b817eebf6a1bd3f
namtuthien <leducmanh123kt@gmail.com>
2023-10-26 22:21:36 +0700

    Fix route for /pages/forgot-password.ejs, /pages/reset-password.ejs

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,8 +1,32 @@
 class SiteController {
+
     // [GET] /
     index(req, res) {
         res.send('trang chu')
     }
+
+    // [GET] /register
+    register(req, res) {
+        const title = 'Đăng ký'
+        res.render('./pages/register.ejs', { title })
+    }
+
+    // [GET] /login
+    login(req, res) {
+        const title = 'Đăng nhập'
+        res.render('./pages/login.ejs', { title })
+    }
+
+    // [GET] /forgot-password
+    forgot(req, res) {
+        res.render('./pages/forgot-password.ejs')
+    }
+
+    // [GET] /reset-password
+    reset(req, res) {
+        res.render('./pages/reset-password.ejs')
+    }
+
 }
 
 module.exports = new SiteController()
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,6 @@
 const siteRouter = require('./siteRouter')
 
 function route(app) {
-    // register
-    app.use('/register', (req, res) => { res.render('./pages/register.ejs') })
-    app.use('/login', (req, res) => { res.render('./pages/login.ejs') })
-    app.use('/forgotPassword', (req, res) => { res.render('./pages/forgotPassword.ejs') })
-    app.use('/resetPassword', (req, res) => { res.render('./pages/resetPassword.ejs') })
     app.use('/', siteRouter)
 }
 
