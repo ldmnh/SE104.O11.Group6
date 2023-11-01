bc1623247cb05608f56950a09cd141c52776228d
namtuthien <leducmanh123kt@gmail.com>
2023-11-02 00:15:01 +0700

    Add /pages/order-details.ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const siteRouter = require('./siteRouter')
 const accountRouter = require('./accountRouter')
 
 function route(app) {
+    app.use('/order-detail', (req, res) => res.render('./pages/order-detail'))
     app.use('/order-confirm', (req, res) => res.render('./pages/order-confirm'))
     app.use('/detail', (req, res) => res.render('./pages/acco-detail'))
     app.use('/account', accountRouter)
