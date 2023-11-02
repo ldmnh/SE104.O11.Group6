6a2d793aa90aec1acb28e46468dfc93b679bcfc1
FredLe09 <le197903@gmail.com>
2023-11-03 02:21:04 +0700

    Update /src/routes/siteRouter.js /src/controllers/SiteController.js 'Add feature [POST] forgot-password and modify [GET] forget-password'

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -43,18 +43,37 @@ class SiteController {
                     res.send(conflictError);
                     // res.render('./pages/site/login.ejs', { title, email, password, conflictError });
                 }
-            });
+            }
+        );
     }
 
-    // [GET] /about-us
+    // [GET] /about
     about(req, res) {
         res.render('./pages/site/about-us')
     }
 
     // [GET] /forgot-password
-    forgot(req, res) {
+    showForgotForm(req, res) {
         const title = 'Nhận liên kết đặt lại mật khẩu'
         res.render('./pages/site/forgot-password', { title })
+    }
+
+    // [POST] /forgot-password
+    forgot(req, res, next) {
+        const { email } = req.body;
+        db.query(
+            'SELECT * FROM authuser WHERE au_user_email = ?',
+            [email],
+            (err, result, fields) => {
+                if (err) throw err;
+                if (result.length > 0) {
+                    res.status(200).json({ message: 'Email đã được gửi' });
+                    next();
+                } else {
+                    res.status(404).json({ message: 'Email không tồn tại' });
+                }
+            }
+        );
     }
 
     // [GET] /reset-password
--- src/routes/siteRouter.js
+++ src/routes/siteRouter.js
@@ -9,7 +9,8 @@ router.get('/about', SiteController.about)
 router.get('/register', SiteController.register)
 router.get('/login', authMiddleware.isAuth, SiteController.showLoginForm)
 router.post('/login', authMiddleware.isAuth, SiteController.login)
-router.get('/forgot-password', SiteController.forgot);
+router.get('/forgot-password', SiteController.showForgotForm);
+router.post('/forgot-password', SiteController.forgot);
 router.get('/reset-password', SiteController.reset);
 router.get('/search-results', SiteController.search);
 
