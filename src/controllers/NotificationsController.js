41b2aac9b5c51e3bf0467cdf7dbda81266c9dd92
[Demo] <[pnhaiyen520@gmail.com]>
2023-11-06 14:54:31 +0700

    Fix /pages/search-result

--- src/controllers/NotificationsController.js
+++ src/controllers/NotificationsController.js
@@ -1,9 +1,4 @@
 class NotificationsController {
-
-    // [GET] /nofitications
-    // index(req, res) {
-    //     res.render('./pages/nofitications.ejs')
-    // }
     accountUpdate(req, res) {
         res.render('./pages/notifications/account-update.ejs')
     }
