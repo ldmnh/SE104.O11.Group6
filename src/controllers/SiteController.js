5687c72034722bddb28f54e7ef53d6be2656a6c0
trinhnamadc <trinhnamadc@gmail.com>
2023-11-08 01:04:28 +0700

    add page booking_info

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,53 +1,67 @@
 class SiteController {
+  // [GET] /
+  index(req, res) {
+    res.render("./pages/site/index");
+  }
 
-    // [GET] /
-    index(req, res) {
-        res.render('./pages/site/index')
-    }
+  // [GET] /register
+  register(req, res) {
+    const title = "Đăng ký";
+    res.render("./pages/site/register", { title });
+  }
 
-    // [GET] /register
-    register(req, res) {
-        const title = 'Đăng ký'
-        res.render('./pages/site/register', { title })
-    }
+  // [GET] /login
+  login(req, res) {
+    const title = "Đăng nhập";
+    res.render("./pages/site/login", { title });
+  }
 
-    // [GET] /login
-    login(req, res) {
-        const title = 'Đăng nhập'
-        res.render('./pages/site/login', { title })
-    }
+  // [GET] /about-us
+  about(req, res) {
+    res.render("./pages/site/about-us");
+  }
+  // [GET] /footer
+  footer(req, res) {
+    res.render("./pages/footer.ejs");
+  }
 
-    // [GET] /about-us
-    about(req, res) {
-        res.render('./pages/site/about-us')
-    }
-    // [GET] /footer
-    footer(req, res) {
-        res.render('./pages/footer.ejs')
-    }
+  // [GET] /forgot-password
+  forgot(req, res) {
+    const title = "Nhận liên kết đặt lại mật khẩu";
+    res.render("./pages/site/forgot-password", { title });
+  }
 
-    // [GET] /forgot-password
-    forgot(req, res) {
-        const title = 'Nhận liên kết đặt lại mật khẩu'
-        res.render('./pages/site/forgot-password', { title })
-    }
+  // [GET] /reset-password
+  reset(req, res) {
+    const title = "Đặt lại mật khẩu";
+    res.render("./pages/site/reset-password", { title });
+  }
 
-    // [GET] /reset-password
-    reset(req, res) {
-        const title = 'Đặt lại mật khẩu'
-        res.render('./pages/site/reset-password', { title })
-    }
+  // [GET] /search-results
+  search(req, res) {
+    const title = "Kết quả tìm kiếm";
+    res.render("./pages/site/search-results", { title });
+  }
 
-    // [GET] /search-results
-    search(req, res) {
-        const title = 'Kết quả tìm kiếm'
-        res.render('./pages/site/search-results', { title })
-    }
+  payment(req, res) {
+    res.render("./pages/payment.ejs");
+  }
 
-    payment(req, res) {
-        res.render('./pages/payment.ejs')
-    }
-
+  // [GET] /booking-info
+  booking_info(req, res) {
+    const nav_tree__data = [
+      {
+        text: "Trang chủ",
+        link: "/",
+      },
+      {
+        text: "Thông tin đặt phòng",
+        link: "/booking_info",
+      },
+    ];
+    const title = "Thông tin đặt phòng";
+    res.render("./pages/booking/info", { title, nav_tree__data });
+  }
 }
 
-module.exports = new SiteController()
+module.exports = new SiteController();
--- src/routes/siteRouter.js
+++ src/routes/siteRouter.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
 // import controller
-const SiteController = require('../controllers/SiteController.js')
+const SiteController = require("../controllers/SiteController.js");
 
-router.get('/about', SiteController.about)
-router.get('/register', SiteController.register)
-router.get('/footer', SiteController.footer)
-router.get('/login', SiteController.login)
-router.get('/forgot-password', SiteController.forgot);
-router.get('/reset-password', SiteController.reset);
-router.get('/search-results', SiteController.search);
-router.get('/payment', SiteController.payment)
-router.get('/', SiteController.index)
+router.get("/about", SiteController.about);
+router.get("/register", SiteController.register);
+router.get("/footer", SiteController.footer);
+router.get("/login", SiteController.login);
+router.get("/forgot-password", SiteController.forgot);
+router.get("/reset-password", SiteController.reset);
+router.get("/search-results", SiteController.search);
+router.get("/payment", SiteController.payment);
+router.get("/", SiteController.index);
+router.get("/booking_info", SiteController.booking_info);
 
-module.exports = router
-
+module.exports = router;
