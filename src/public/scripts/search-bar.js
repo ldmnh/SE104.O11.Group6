e763b67d1af1b6189083ea5a3067455554d34ca8
Xuan Nhi <xuannhi27.work@gmail.com>
2023-11-09 02:22:37 +0700

    Up search-bar and fix ui header-full, header-medium, header-small, header-small-title

--- src/public/scripts/search-bar.js
+++ src/public/scripts/search-bar.js
@@ -18,3 +18,65 @@ $("#dateRange").on("apply.daterangepicker", function (ev, picker) {
 $("#dateRange").on("cancel.daterangepicker", function (ev, picker) {
   $(this).val("");
 });
+
+//Nút tăng giảm số lượng
+
+const plusButtons = document.querySelectorAll(".plus");
+const minusButtons = document.querySelectorAll(".minus");
+
+// Thêm sự kiện cho nút +
+plusButtons.forEach((plusButton) => {
+  plusButton.addEventListener("click", () => {
+    // Khai báo và lấy tham chiếu đến trường nhập số lượng tương ứng
+    const input = plusButton.parentElement.querySelector(".quantity");
+    const currentValue = Number(input.value);
+
+    if (currentValue < 100) {
+      input.value = currentValue + 1;
+      updateSelectedOptions();
+    }
+  });
+});
+
+// Thêm sự kiện cho nút -
+minusButtons.forEach((minusButton) => {
+  minusButton.addEventListener("click", () => {
+    // Khai báo và lấy tham chiếu đến trường nhập số lượng tương ứng
+    const input = minusButton.parentElement.querySelector(".quantity");
+    const currentValue = Number(input.value);
+    if (currentValue > 0) {
+      input.value = currentValue - 1;
+      updateSelectedOptions();
+    }
+  });
+});
+
+// Hàm cập nhật thông tin đã chọn
+const selectedOptions = document.getElementById("selected-options");
+function updateSelectedOptions() {
+  const adults = document.getElementById("adult__quantity").value;
+  const children = document.getElementById("child__quantity").value;
+  const rooms = document.getElementById("room__quantity").value;
+
+  selectedOptions.innerText = `${adults} người lớn - ${children} trẻ em - ${rooms} phòng`;
+}
+
+// Gọi hàm để cập nhật thông tin đã chọn khi trang web được tải
+updateSelectedOptions();
+
+// Sự kiện khi người dùng click vào phần tử .search-address
+document
+  .querySelector(".search-address--dropdown")
+  .addEventListener("click", function () {
+    // Lấy tham chiếu đến phần tử .search-quality
+    const searchQuality = document.querySelector(".search-quality");
+
+    // Kiểm tra xem .search-quality đang hiển thị hay không
+    if (searchQuality.style.visibility === "visible") {
+      searchQuality.style.visibility = "hidden";
+      searchQuality.style.opacity = 0;
+    } else {
+      searchQuality.style.visibility = "visible";
+      searchQuality.style.opacity = 1;
+    }
+  });
