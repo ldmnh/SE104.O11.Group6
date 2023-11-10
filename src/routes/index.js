4149cf9e9b6de21ba07445c4170cebb798c332d8
FredLe09 <le197903@gmail.com>
2023-11-11 03:24:21 +0700

    Format code /src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,27 @@
 // import router
-const authRouter = require('./authRouter')
-const searchRouter = require('./searchRouter')
-const notificationRouter = require('./notificationRouter')
-const accountRouter = require('./accountRouter')
-const bookingRouter = require('./bookingRouter')
-const siteRouter = require('./siteRouter')
+const authRouter = require("./authRouter")
+const searchRouter = require("./searchRouter")
+const notificationRouter = require("./notificationRouter")
+const accountRouter = require("./accountRouter")
+const bookingRouter = require("./bookingRouter")
+const siteRouter = require("./siteRouter")
 
-function route(app) {
-    app.use('/auth', authRouter)
-    app.use('/search', searchRouter)
-    app.use('/notification', notificationRouter)
-    app.use('/account', accountRouter)
-    app.use('/booking', bookingRouter)
-    app.use('/', siteRouter)
-    app.get('/testing', (req, res) => {
-        req.session.loggedin = true
-        req.session.email = 'john.doe@example.com'
+const route = (app) => {
+    app.use("/auth", authRouter)
+    app.use("/search", searchRouter)
+    app.use("/notification", notificationRouter)
+    app.use("/account", accountRouter)
+    app.use("/booking", bookingRouter)
+    app.use("/", siteRouter)
+    app.get("/testing", (req, res) => {
+        req.session.user = {
+            "isLoggedIn": true,
+            "email": "john.doe@example.com"
+        }
         res.status(200).json({
-            loggedin: req.session.loggedin,
-            email: req.session.email,
-            message: 'OK'
+            loggedin: req.session.user?.isLoggedIn,
+            email: req.session.user?.email,
+            message: "OK"
         })
     })
 }
