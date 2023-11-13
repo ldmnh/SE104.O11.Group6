ad47587d70aaa45105c73910b4cdcab41ab8599e
FredLe09 <le197903@gmail.com>
2023-11-14 03:11:15 +0700

    Format code in controllers/DetailController.js

--- src/controllers/DetailController.js
+++ src/controllers/DetailController.js
@@ -4,25 +4,19 @@ class DetailController {
 
     // [GET] /
     showReview(req, res) {
-        if (!req.session.email) {
-            res.status(404).json({ message: 'Không tìm thấy email!!!' });
-            return;
-        }
-
         const sql = `
-        SELECT
-            B.AU_USER_FIRST_NAME AS 'AU_USER_FIRST_NAME',
-            B.AU_USER_LAST_NAME AS 'AU_USER_LAST_NAME',
-            A.RATING_DATETIME AS 'RATING_DATETIME',
-            A.RATING_CONTEXT AS 'RATING_CONTEXT',
-            A.RATING_POINT AS 'RATING_POINT'
-        FROM view_RATING AS A
-        INNER JOIN AUTHUSER AS B ON A.au_user_id = B.au_user_id
-        INNER JOIN ROOMTYPE AS C ON A.room_id = C.room_id
-        INNER JOIN ACCOMMODATION AS D ON D.acco_id = C.acco_id
-        WHERE B.au_user_email = ?
-        `;
-        const params = [req.session.email];
+            SELECT
+                B.AU_USER_FIRST_NAME AS 'AU_USER_FIRST_NAME',
+                B.AU_USER_LAST_NAME AS 'AU_USER_LAST_NAME',
+                A.RATING_DATETIME AS 'RATING_DATETIME',
+                A.RATING_CONTEXT AS 'RATING_CONTEXT',
+                A.RATING_POINT AS 'RATING_POINT'
+            FROM view_RATING AS A
+            INNER JOIN AUTHUSER AS B ON A.au_user_id = B.au_user_id
+            INNER JOIN ROOMTYPE AS C ON A.room_id = C.room_id
+            INNER JOIN ACCOMMODATION AS D ON D.acco_id = C.acco_id
+            WHERE B.au_user_email = ?`;
+        const params = [req.session.user?.email];                     // Nên đổi thành id
 
         db.query(sql, params, (err, result, fields) => {
             if (err) {
@@ -35,7 +29,7 @@ class DetailController {
                     message: 'Lấy thông tin đánh giá thành công',
                     data: result
                 });
-                console.log(result);
+                console.log(result);   // Nên xóa 
             } else {
                 res.status(404).json({ message: 'Không tìm thấy tài khoản!!!', });
             }
