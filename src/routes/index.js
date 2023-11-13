048ed3dd4d895551dcffe632d7aa0b0fd888f4a8
FredLe09 <le197903@gmail.com>
2023-11-14 03:07:29 +0700

    Format code controllers/AccountController.js routers/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 // import router
+const accountRouter = require("./accountRouter")
 const adminRouter = require("./adminRouter")
 const authRouter = require("./authRouter")
-const searchRouter = require("./searchRouter")
-const notificationRouter = require("./notificationRouter")
-const accountRouter = require("./accountRouter")
 const bookingRouter = require("./bookingRouter")
+const notificationRouter = require("./notificationRouter")
+const searchRouter = require("./searchRouter")
 const siteRouter = require("./siteRouter")
 
 const route = (app) => {
