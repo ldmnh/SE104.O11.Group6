a8ea58331e1c17e0d025eccbb58a1a8c309e63bf
LamCam <lamcam813@gmail.com>
2023-11-15 21:56:13 +0700

    Update backend notifications

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -72,16 +72,4 @@ adminModel.getChartRating = function (callback) {
     })
 }
 
-module.exports = adminModel;// [PUT] /account/delBank
-AccountModel.delBank = ({ id, bank_id }, callback) => {
-    const sql = `
-            UPDATE bankcard
-            SET au_user_id = NULL
-            WHERE bank_id=?
-            and au_user_id=${id}
-        `
-    const params = [bank_id];
-    db.query(sql, params, (err, result) => {
-        callback(err, result);
-    })
-}
+module.exports = adminModel;
