e06eed6e5add60a1fa25adc6a24025eccbb7f73b
LY PHI LAN <21520319@gm.uit.edu.vn>
2023-11-17 04:08:48 +0700

    update branch lpl

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,19 +1,30 @@
 const db = require("../config/db/connect");
-const SearchModel = require("../models/searchModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const accoRoomDetail = require("../models/AccoRoomDetail.model");
 
 class SearchController {
-    // [GET] /search/results
-    searchResult(req, res) {
-        const {
-            location, checkIn, checkOut, adult, child, room,
-            acco_type, rating_point, bed_type, acco_star, cost,
-            accoStar, countRating,
-        } = req.query;
-        let sql = `
+  // [GET] /search/results
+  searchResult(req, res) {
+    const {
+      location,
+      checkIn,
+      checkOut,
+      adult,
+      child,
+      room,
+      acco_type,
+      rating_point,
+      bed_type,
+      acco_star,
+      acco_fea,
+      price,
+      cost,
+      accoStar,
+      countRating,
+    } = req.query;
+    let sql = `
         SELECT X.room_id
             FROM 
             (
@@ -31,7 +42,7 @@ class SearchController {
                         ) AS B
                     ON A.prov_id = B.prov_id
 
-    //             UNION
+                    UNION
 
                     SELECT acco_id
                     FROM accommodation AS C
@@ -84,133 +95,159 @@ class SearchController {
             ) AS Y
             ON X.room_id = Y.room_id`;
 
-        const searchQuery = `%${location}%`
-        const params = [
-            searchQuery, searchQuery, adult, child, checkIn, checkIn,
-            checkOut, checkOut, checkIn, checkOut, room,
-        ];
+    const searchQuery = `%${location}%`;
+    const params = [
+      searchQuery,
+      searchQuery,
+      adult,
+      child,
+      checkIn,
+      checkIn,
+      checkOut,
+      checkOut,
+      checkIn,
+      checkOut,
+      room,
+    ];
 
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                res.status(500).json({ message: 'Lỗi truy cập cơ sở dữ liệu' })
-                throw err
-            }
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(500).json({ message: "Lỗi truy cập cơ sở dữ liệu" });
+        throw err;
+      }
 
-            if (result.length > 0) {
-                res.status(200).json({
-                    message: "Đã tìm thành công",
-                    data: result,
-                });
-                let sql1 = `SELECT * FROM accommodation, typeroom WHERE accommodation.acco_id=typeroom.acco_id AND typeroom.room_id IN ${data.room_id.join(
-                    ","
-                )}`;
-                if (acco_type) {
-                    let acco_typeFilter = acco_type.join("','");
-                    sql1 += ` AND acco_type IN ('${acco_typeFilter}')`;
-                }
+      if (result.length > 0) {
+        // res.status(200).json({
+        //     message: "Đã tìm thành công",
+        //     data: result,
+        // });
+        let resultFilter = result.map((obj) => obj.room_id).join(",");
+        let sql1 = `SELECT DISTINCT A.acco_id, R.room_id, A.acco_star, R.room_date_end_discount, A.acco_name, R.room_avg_rating, R.room_count_rating, A.acco_location_link, R.room_class, R.room_max_adult, R.room_type, R.room_cost, R.room_discount, A.acco_tiny_img_url, R.room_single_bed, R.room_double_bed FROM accommodation as A, roomtype as R, accofea as AF WHERE A.acco_id = R.acco_id AND AF.acco_id = A.acco_id AND R.room_id IN (${resultFilter})`;
 
-                if (rating_point == "9+") sql1 += ` AND rating_point BETWEEN 9 AND 10`;
-                if (rating_point == "8+") sql1 += ` AND rating_point BETWEEN 8 AND 9`;
-                if (rating_point == "7+") sql1 += ` AND rating_point BETWEEN 7 AND 8`;
-                if (rating_point == "6+") sql1 += ` AND rating_point BETWEEN 6 AND 7`;
+        if (price == "Dưới VND 200.000")
+          sql1 += ` AND room_cost BETWEEN 0 AND 200000`;
+        if (price == "VND 200.000 - VND 500.000")
+          sql1 += ` AND room_cost BETWEEN 200000 AND 500000`;
+        if (price == "VND 500.000 - VND 1.000.000")
+          sql1 += ` AND room_cost BETWEEN 500000 AND 1000000`;
+        if (price == "Trên VND 1.000.000") sql1 += ` AND room_cost > 1000000`;
 
-                if (bed_type == "Giường đơn") {
-                    sql1 += ` AND room_single_bed > 0`;
-                }
-                if (bed_type == "Giường đôi") {
-                    sql1 += ` AND room_double_bed > 0`;
-                }
+        if (acco_type) {
+          let acco_typeFilter = acco_type.join(",");
+          sql1 += ` AND acco_type IN ('${acco_typeFilter}')`;
+        }
 
-                // if (distance_center == "Dưới 1km")
-                //     sql += ` AND distance_center BETWEEN 0 AND 1`
-                // if (distance_center == "Dưới 3km")
-                //     sql += ` AND distance_center BETWEEN 0 AND 3`
-                // if (distance_center == "Dưới 5km")
-                //     sql += ` AND distance_center BETWEEN 0 AND 5`
+        if (rating_point == "9+")
+          sql1 += ` AND room_avg_rating BETWEEN 9 AND 10`;
+        if (rating_point == "8+")
+          sql1 += ` AND room_avg_rating BETWEEN 8 AND 10`;
+        if (rating_point == "7+")
+          sql1 += ` AND room_avg_rating BETWEEN 7 AND 10`;
+        if (rating_point == "6+")
+          sql1 += ` AND room_avg_rating BETWEEN 6 AND 10`;
 
-                // if (meal == "ext000000001") {
-                //     sql += ` AND roomexte.exte_id == "ext000000001"`
-                // }
+        if (bed_type == "Giường đơn") {
+          sql1 += ` AND room_single_bed > 0`;
+        }
+        if (bed_type == "Giường đôi") {
+          sql1 += ` AND room_double_bed > 0`;
+        }
 
-                if (acco_star) {
-                    let acco_starFilter = acco_star.join("','");
-                    sql1 += ` AND acco_star IN ('${acco_starFilter}')`;
-                }
+        // if (distance_center == "Dưới 1km")
+        //     sql += ` AND distance_center BETWEEN 0 AND 1`
+        // if (distance_center == "Dưới 3km")
+        //     sql += ` AND distance_center BETWEEN 0 AND 3`
+        // if (distance_center == "Dưới 5km")
+        //     sql += ` AND distance_center BETWEEN 0 AND 5`
 
-                // if (acco_fea) {
-                //     let acco_feaFilter = acco_fea.join("','")
-                //     sql += ` AND accofea.fea_id IN ('${acco_feaFilter}')`
-                // }
+        // if (meal == "ext000000001") {
+        //     sql += ` AND roomexte.exte_id == "ext000000001"`
+        // }
 
-                // if (acco_exte) {
-                //     let acco_exteFilter = acco_exte.join("','")
-                //     sql += ` AND roomexte.exte_id IN ('${acco_exteFilter}')`
-                // }
-                // if (room_exte) {
-                //     let room_exteFilter = room_exte.join("','")
-                //     sql += ` AND roomexte.exte_id IN ('${room_exteFilter}')`
-                // }
+        if (acco_star) {
+          let acco_starFilter = acco_star.join(",");
+          sql1 += ` AND acco_star IN ('${acco_starFilter}')`;
+        }
 
-                if (cost == "Cao đến thấp")
-                    // result.sort((a, b) => a.room_cost - b.room_cost)
-                    sql1 += ` ORDER BY room_cost DESC`;
-                if (cost == "Thấp đến cao") sql1 += ` ORDER BY room_cost ASC`;
-                if (accoStar == "Cao đến thấp") sql1 += ` ORDER BY acco_star DESC`;
-                if (accoStar == "Thấp đến cao") sql1 += ` ORDER BY acco_star ASC`;
-                if (countRating == "Cao đến thấp")
-                    sql1 += ` ORDER BY room_sum_rating DESC`;
-                if (countRating == "Thấp đến cao")
-                    sql1 += ` ORDER BY room_sum_rating ASC`;
-                db.query(sql1, (err, result1) => {
-                    if (err) {
-                        res.status(500).json({ message: "Lỗi truy cập cơ sở dữ liệu" });
-                        throw err;
-                    }
+        if (acco_fea) {
+          let acco_feaFilter = acco_fea.join(",");
+          sql += ` AND accofea.fea_id IN ('${acco_feaFilter}')`;
+        }
 
-                    if (result1.length > 0) {
-                        res.status(200).json({
-                            message: "Đã tìm thành công",
-                            data: result1,
-                        });
-                    } else {
-                        res.status(404).json({ message: "Không tìm thấy kết quả" });
-                    }
-                });
+        // if (acco_exte) {
+        //     let acco_exteFilter = acco_exte.join("','")
+        //     sql += ` AND roomexte.exte_id IN ('${acco_exteFilter}')`
+        // }
+        // if (room_exte) {
+        //     let room_exteFilter = room_exte.join("','")
+        //     sql += ` AND roomexte.exte_id IN ('${room_exteFilter}')`
+        // }
 
-            } else {
-                res.status(404).json({ message: "Không tìm thấy kết quả" });
-            }
+        if (cost == "Cao đến thấp")
+          // result.sort((a, b) => a.room_cost - b.room_cost)
+          sql1 += ` ORDER BY room_cost DESC`;
+        if (cost == "Thấp đến cao") sql1 += ` ORDER BY room_cost ASC`;
+        if (accoStar == "Cao đến thấp") sql1 += ` ORDER BY acco_star DESC`;
+        if (accoStar == "Thấp đến cao") sql1 += ` ORDER BY acco_star ASC`;
+        if (countRating == "Cao đến thấp")
+          sql1 += ` ORDER BY room_sum_rating DESC`;
+        if (countRating == "Thấp đến cao")
+          sql1 += ` ORDER BY room_sum_rating ASC`;
+        db.query(sql1, (err, result1) => {
+          if (err) {
+            res.status(500).json({
+              message: "Lỗi truy cập cơ sở dữ liệu",
+            });
+            throw err;
+          }
+
+          if (result1.length > 0) {
+            res.status(200).render("./pages/search/results", {
+              // res.send({
+              message: "Đã tìm thành công",
+              data: result1,
+            });
+          } else {
+            res.status(404).json({
+              message: "Không tìm thấy kết quả",
+            });
+          }
         });
-    }
+      } else {
+        res.status(404).json({ message: "Không tìm thấy kết quả" });
+      }
+    });
+  }
 
-    // [GET] /search/:acco_id
-    accoDetail(req, res) {
-        accoRoomDetail.getDetail(req, res, function (err, accoDetail, accoFea, accoImg, accoRoom, accoRoomRating) {
-            if (!accoDetail[0]) {
-                res.status(404).render('./pages/site/error404.ejs')
-            }
-            res.status(200).render('./pages/search/detail', {
-                // res.status(200).json({
-                message: 'Lấy thông tin thành công',
-                accoDetail: accoDetail,
-                accoFea: accoFea,
-                accoImg: accoImg,
-                accoRoom: accoRoom,
-                accoRoomRating: accoRoomRating,
-            })
-        })
-        // res.status(200).render('./pages/search/detail')
-    }
+  // [GET] /search/:acco_id
+  accoDetail(req, res) {
+    accoRoomDetail.getDetail(
+      req,
+      res,
+      function (err, accoDetail, accoFea, accoImg, accoRoom, accoRoomRating) {
+        res.status(200).render("./pages/search/detail", {
+          // res.status(200).json({
+          message: "Lấy thông tin thành công",
+          accoDetail: accoDetail,
+          accoFea: accoFea,
+          accoImg: accoImg,
+          accoRoom: accoRoom,
+          accoRoomRating: accoRoomRating,
+        });
+      }
+    );
+    // res.status(200).render('./pages/search/detail')
+  }
 
-    // [POST] /search:acco_id
-    submitBooking(req, res) {
-        console.log(req.body)
-        const { acco_id, room_id, room_number, room_cost_before, room_cost_after } = req.body;
+  // [POST] /search:acco_id
+  submitBooking(req, res) {
+    const { acco_id, room_id, room_number, room_cost_before, room_cost_after } =
+      req.body;
 
     req.session.acco = { id: acco_id };
 
     req.session.rooms = room_number
-      .map((value, index) => {  
+      .map((value, index) => {
         return {
           id: Number(room_id[index]),
           num: Number(value),
@@ -220,8 +257,8 @@ class SearchController {
       })
       .filter((value) => value.num > 0);
 
-        res.redirect('/booking/information');
-    }
+    // res.redirect('/booking/information');
+  }
 }
 
 module.exports = new SearchController();
--- src/public/scripts/search-results.js
+++ src/public/scripts/search-results.js
@@ -1,105 +1,154 @@
-// Điều hướng nút bản đồ
-const mapBtn = document.getElementById('btn-map')
-const redirect2map = () => {
-    window.location.href = 'https://maps.app.goo.gl/GbwxfoKVzvYoN1hn9'
-}
-
-mapBtn.addEventListener('click', redirect2map)
-
-// Điều hướng nút xem ngay
-const detailBtn = document.querySelectorAll('.form__submit')
-
-detailBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
-        window.location.href = `http://127.0.0.1:3000/search/:acco_id`
-    })
-})
-
-
-
 // Lấy danh sách tất cả các thành phần search-results__filter__item
-const filterItems = document.querySelectorAll('.search-results__filter__item')
+const filterItems = document.querySelectorAll(".search-results__filter__item");
 
 // Lặp qua từng thành phần và thêm sự kiện click
-filterItems.forEach(item => {
-    item.addEventListener('click', () => {
-        // Đặt lại màu nền, màu chữ và font weight của tất cả các thành phần về trạng thái gốc
-        filterItems.forEach(originalItem => {
-            originalItem.style.backgroundColor = '' // Đặt lại màu nền
-            originalItem.style.color = '' // Đặt lại màu chữ
-            originalItem.style.fontWeight = '' // Đặt lại font weight
-        })
+filterItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    // Đặt lại màu nền, màu chữ và font weight của tất cả các thành phần về trạng thái gốc
+    filterItems.forEach((originalItem) => {
+      originalItem.style.backgroundColor = ""; // Đặt lại màu nền
+      originalItem.style.color = ""; // Đặt lại màu chữ
+      originalItem.style.fontWeight = ""; // Đặt lại font weight
+    });
 
-        // Thiết lập màu nền, màu chữ và font weight của thành phần hiện tại
-        item.style.backgroundColor = 'orange' // Màu cam
-        item.style.color = 'white' // Màu trắng
-        item.style.fontWeight = '500' // Font weight 500
-    })
-})
+    // Thiết lập màu nền, màu chữ và font weight của thành phần hiện tại
+    item.style.backgroundColor = "orange"; // Màu cam
+    item.style.color = "white"; // Màu trắng
+    item.style.fontWeight = "500"; // Font weight 500
+  });
+});
 
-let dropdownButtons = document.querySelectorAll('.dropdown-button')
-console.log(dropdownButtons)
+let dropdownButtons = document.querySelectorAll(".dropdown-button");
+console.log(dropdownButtons);
 
-document.body.addEventListener('click', function (event) {
-    let target = event.target
-    if (target.classList.contains('dropdown-button')) {
-        let dropdownContent = target.nextElementSibling
-        if (dropdownContent.style.display === 'block') {
-            dropdownContent.style.display = 'none'
-        } else {
-            // Đóng rồi mới mở cái mới
-            closeAllDropdowns()
-            dropdownContent.style.display = 'block'
-        }
-    } else if (!target.classList.contains('dropdown-content')) {
-        // Đóng hết khi bấm ra ngoài
-        closeAllDropdowns()
+document.body.addEventListener("click", function (event) {
+  let target = event.target;
+  if (target.classList.contains("dropdown-button")) {
+    let dropdownContent = target.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      // Đóng rồi mới mở cái mới
+      closeAllDropdowns();
+      dropdownContent.style.display = "block";
     }
-})
+  } else if (!target.classList.contains("dropdown-content")) {
+    // Đóng hết khi bấm ra ngoài
+    closeAllDropdowns();
+  }
+});
 
 // Đóng hết
 function closeAllDropdowns() {
-    let dropdownContents = document.querySelectorAll('.dropdown-content')
-    dropdownContents.forEach(function (content) {
-        content.style.display = 'none'
-    })
+  let dropdownContents = document.querySelectorAll(".dropdown-content");
+  dropdownContents.forEach(function (content) {
+    content.style.display = "none";
+  });
 }
 
 // Mặc định chọn bộ lọc theo 'Giá'
-const defaultSelectedItem = document.getElementById('filter__item-price')
-defaultSelectedItem.click()
+const defaultSelectedItem = document.getElementById("filter__item-price");
+defaultSelectedItem.click();
 
 // Lấy các phần tử cần tương tác
-const extendContent = document.getElementById('extend-content')
-const toggleExtend = document.getElementById('toggle-extend')
-const hiddenCheckboxes = document.querySelectorAll('.form-check.hidden')
-let isExpanded = false
+const extendContent = document.getElementById("extend-content");
+const toggleExtend = document.getElementById("toggle-extend");
+const hiddenCheckboxes = document.querySelectorAll(".form-check.hidden");
+let isExpanded = false;
 
 // Lắng nghe sự kiện click trên nút 'Hiển thị tất cả' và 'Ấn bớt'
-toggleExtend.addEventListener('click', function (e) {
-    e.preventDefault()
+toggleExtend.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    // Nếu đang ẩn, thì hiển thị tất cả
-    if (!isExpanded) {
-        hiddenCheckboxes.forEach(item => {
-            item.classList.remove('hidden')
-        })
+  // Nếu đang ẩn, thì hiển thị tất cả
+  if (!isExpanded) {
+    hiddenCheckboxes.forEach((item) => {
+      item.classList.remove("hidden");
+    });
 
-        // Thay đổi nội dung và biểu tượng của nút
-        toggleExtend.textContent = 'Ẩn bớt'
-        extendContent.querySelector('span').textContent = 'expand_less'
+    // Thay đổi nội dung và biểu tượng của nút
+    toggleExtend.textContent = "Ẩn bớt";
+    extendContent.querySelector("span").textContent = "expand_less";
 
-        isExpanded = true
-    } else {
-        // Nếu đang hiển thị tất cả, thì ẩn bớt
-        hiddenCheckboxes.forEach(item => {
-            item.classList.add('hidden')
-        })
+    isExpanded = true;
+  } else {
+    // Nếu đang hiển thị tất cả, thì ẩn bớt
+    hiddenCheckboxes.forEach((item) => {
+      item.classList.add("hidden");
+    });
 
-        // Thay đổi nội dung và biểu tượng của nút
-        toggleExtend.textContent = 'Hiển thị tất cả'
-        extendContent.querySelector('span').textContent = 'expand_more'
+    // Thay đổi nội dung và biểu tượng của nút
+    toggleExtend.textContent = "Hiển thị tất cả";
+    extendContent.querySelector("span").textContent = "expand_more";
 
-        isExpanded = false
-    }
-})
+    isExpanded = false;
+  }
+});
+
+document.addEventListener("click", function (e) {
+  const target = e.target;
+  if (target.classList.contains("form-check-input")) {
+    filterSortData();
+  }
+  if (target.classList.contains("sort")) {
+    filterSortData();
+  }
+});
+filterSortData();
+function filterSortData() {
+  const acco_type = getFilter("acco_type");
+  const rating_point = getFilter("rating_point");
+  const bed_type = getFilter("bed_type");
+  const acco_star = getFilter("acco_star");
+  const acco_fea = getFilter("acco_fea");
+  const price = getFilter("price");
+  const cost = getSort("cost");
+  const accoStar = getSort("accoStar");
+  const countRating = getSort("countRating");
+
+  $.ajax({
+    url: "/search/results",
+    method: "GET",
+    data: {
+      acco_type,
+      rating_point,
+      bed_type,
+      acco_star,
+      acco_fea,
+      price,
+      cost,
+      accoStar,
+      countRating,
+    },
+    success: function (result) {
+      if (result.message == "success") {
+        const accoList = result.data;
+        // displayAccoList(accoList);
+        console.log(accoList);
+      } else {
+        // notFound();
+        console.log("Không tìm thấy");
+      }
+    },
+  });
+}
+
+function getFilter(className) {
+  let filter = [];
+  let formCheckInput = document.querySelectorAll(
+    `.${className}.form-check-input:checked`
+  );
+  formCheckInput.forEach((item) => {
+    filter.push(item.value);
+  });
+  return filter;
+}
+function getSort(className) {
+  let sort = document.querySelectorAll(`.${className}.sort:checked`);
+  let sortResult = null;
+  if (sort.length > 0) {
+    sortResult = sort[0].value;
+  }
+  // sort.classList.remove('checked')
+  return sortResult;
+}
