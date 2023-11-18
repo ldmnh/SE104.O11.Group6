b8cd8b3476e84582f7d18220ce84ece0dba9cebf
FredLe09 <le197903@gmail.com>
2023-11-18 16:18:10 +0700

    Fix accountHistory.model

--- src/models/accountHistory.model.js
+++ src/models/accountHistory.model.js
@@ -4,32 +4,35 @@ const jwt = require('jsonwebtoken')
 
 const accountHistory = function () { }
 
-accountHistory.getDetail = function (req, res, callback) {
-    const getBookingDetail = 'SELECT * FROM view_booking_history WHERE au_user_id = ?'
-    const params = [req.session.user.au_user_id]
-    db.query(getBookingDetail, [params], function (err, bookingDetails) {
-        bookingDetails.forEach(function (booking) {
-            booking.book_datetime = booking.book_datetime.getDate() +'/'+ booking.book_datetime.getMonth()+'/'+ booking.book_datetime.getYear()
-            booking.book_start_datetime = booking.book_start_datetime.getDate() +'/'+ booking.book_start_datetime.getMonth()+'/'+ booking.book_start_datetime.getYear()
-            booking.book_end_datetime = booking.book_end_datetime.getDate() +'/'+ booking.book_end_datetime.getMonth()+'/'+ booking.book_end_datetime.getYear()
-            
-            booking.book_cost_after_currency = booking.book_cost_after.toFixed(0).replace(/./g, function(c, i, a) {
-                            return i > 0 && c !== "," && (a.length - i) % 3 === 0 ? "." + c : c;
-                          });
-            booking.book_cost_before_currency = booking.book_cost_before.toFixed(0).replace(/./g, function(c, i, a) {
-                            return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
-                          });
-            
-            switch (booking.book_status){
-                case -1: 
-                booking.book_status_msg = 'Đã hủy'
-                break
-                
-                case 0: 
+accountHistory.getDetail = function ({ id }, callback) {
+    const getBookingDetail = `
+        SELECT *
+        FROM view_booking_history
+        WHERE au_user_id = ${id}`;
+
+    db.query(getBookingDetail, (err, bookingDetails) => {
+        bookingDetails?.forEach((booking) => {
+            booking.book_datetime = booking.book_datetime.getDate() + '/' + booking.book_datetime.getMonth() + '/' + booking.book_datetime.getYear()
+            booking.book_start_datetime = booking.book_start_datetime.getDate() + '/' + booking.book_start_datetime.getMonth() + '/' + booking.book_start_datetime.getYear()
+            booking.book_end_datetime = booking.book_end_datetime.getDate() + '/' + booking.book_end_datetime.getMonth() + '/' + booking.book_end_datetime.getYear()
+
+            booking.book_cost_after_currency = booking.book_cost_after.toFixed(0).replace(/./g, function (c, i, a) {
+                return i > 0 && c !== "," && (a.length - i) % 3 === 0 ? "." + c : c;
+            });
+            booking.book_cost_before_currency = booking.book_cost_before.toFixed(0).replace(/./g, function (c, i, a) {
+                return i > 0 && c !== "." && (a.length - i) % 3 === 0 ? "," + c : c;
+            });
+
+            switch (booking.book_status) {
+                case -1:
+                    booking.book_status_msg = 'Đã hủy'
+                    break
+
+                case 0:
                     booking.book_status_msg = 'Chưa sử dụng'
                     break
-             
-                case 1: 
+
+                case 1:
                     booking.book_status_msg = 'Đã sử dụng'
                     break
 
@@ -38,12 +41,12 @@ accountHistory.getDetail = function (req, res, callback) {
                     break
             }
 
-            switch (booking.book_is_paid){
-                case 0: 
+            switch (booking.book_is_paid) {
+                case 0:
                     booking.book_is_paid_msg = 'Chưa thanh toán'
                     break
-             
-                case 1: 
+
+                case 1:
                     booking.book_is_paid_msg = 'Đã thanh toán'
                     break
 
@@ -51,9 +54,8 @@ accountHistory.getDetail = function (req, res, callback) {
                     booking.book_is_paid_msg = 'Đang xử lý'
                     break
             }
-        
         })
-        callback(err, req.session.user, bookingDetails)
+        callback(err, bookingDetails)
     })
 }
 
