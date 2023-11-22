bfe9609d7f7b2adeb3f595db6a3fd454412da1a5
namtuthien <leducmanh123kt@gmail.com>
2023-11-22 15:18:27 +0700

    Add /admin/login

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // import controller
 const AdminController = require('../controllers/AdminController.js')
 
+router.get('/login', AdminController.login)
 router.get('/dashboard', AdminController.dashboard)
 router.get('/dashboard/getchart', AdminController.getChart)
 
